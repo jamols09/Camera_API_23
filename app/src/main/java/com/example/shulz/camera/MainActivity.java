37221c24ff6eb1ac874aa45142c11ec0fee5378d
ovmaglana <jamols09>
2016-07-05 10:09:04 +0800

    Added public methods cameraIntent,galleryIntent

--- app/src/main/java/com/example/shulz/camera/MainActivity.java
+++ app/src/main/java/com/example/shulz/camera/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.shulz.camera;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         build.setItems(options, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                boolean result = Utility.checkPermission(MainActivity.this);
+                boolean result = Utility.checkPermission(MainActivity.this); //request permission of application
                 if (options[i].equals("Take Photo")){
                     userChoosenTask="Take Photo";
                     if(result){
@@ -42,5 +44,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         build.show();
+    }
+
+    private void cameraIntent()//open the camera of user
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//launch the existing camera of user
+        startActivityForResult(intent, 0);//call
+    }
+
+    private void galleryIntent() //runs on version 3.0 and above (honeycomb)
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Image"),SELECT_FILE);
     }
 }
